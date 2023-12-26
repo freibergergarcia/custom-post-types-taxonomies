da4504db87179c2f3e0197cb0287fe71185fda6f
Bruno FG <freibergergarcia@gmail.com>
2023-12-26 18:11:33 +1100

    add php doc blocks to template header

--- includes/Admin/templates/custom-ptt-header.php
+++ includes/Admin/templates/custom-ptt-header.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * Custom Post Types & Taxonomies header template.
+ */
+?>
 <div class="bg-white shadow px-4 py-4 w-full text-center">
 	<h1 class="text-2xl font-bold">Custom Post Types & Taxonomies</h1>
 </div>
